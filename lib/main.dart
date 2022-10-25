d6f05b8ae9445166c282a0c3a85a8ee311023ed5
Yoochanhong <chanhongy6@naver.com>
2022-10-25 23:19:03 +0900

    🤏 :: stateless 위젯 >> stateful 위젯으로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:wwhat_android/screen/mainpage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
